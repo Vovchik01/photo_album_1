a0d80472d078bee44b93b49d774ce33d61ad525e
Vovchik01 <wavilla23@gmail.com>
2021-12-29 11:57:33 +0300

    initial commit

---
+++ photo_album/base/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {}
+    return render(request, 'base/index.html', context=context)
+
+
+def view_photo(request):
+    context = {}
+    return render(request, 'base/view_photo.html', context=context)
+
+
+def add_photo(request):
+    context = {}
+    return render(request, 'base/add_photo.html', context=context)
