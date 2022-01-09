655ff75eed953e9cd6f66aa9ef0bc6bbf872dbbc
Vovchik01 <wavilla23@gmail.com>
2022-01-09 18:44:04 +0300

    added form to add images

--- photo_album/base/views.py
+++ photo_album/base/views.py
@@ -1,16 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 
 def index(request):
-    context = {}
+    categories = Category.objects.all()
+    photos = Photo.objects.all()
+    context = {'categories': categories, 'photos': photos}
     return render(request, 'base/index.html', context=context)
 
 
 def view_photo(request, pk):
-    context = {}
+    photo = Photo.objects.get(id=pk)
+    context = {'photo': photo}
     return render(request, 'base/view_photo.html', context=context)
 
 
 def add_photo(request):
-    context = {}
+    categories = Category.objects.all()
+
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['category'] != 'none':
+            category = Category.objects.get(id=data['category'])
+        elif data['category_new'] != '':
+            category, created = Category.objects.get_or_create(name=data['category_new'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category,
+            description=data['description'],
+            image=image,
+        )
+
+        return redirect('index')
+
+    context = {'categories': categories, }
     return render(request, 'base/add_photo.html', context=context)
